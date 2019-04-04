f346827117e0813ceefde4d821873746cb5a5b2c
Steven Franzen <sfranzen85@gmail.com>
2019-04-04 22:50:14 +0200

    Test ISMTCS against a random player
    The test program now plays a number of games and reports the number of
    times the ISMCTS player won, in addition to the time spent searching
    trees.

--- test/main.cpp
+++ test/main.cpp
@@ -9,31 +9,59 @@
 #include "card.h"
 
 #include <chrono>
+#include <vector>
+#include <random>
 #include <iostream>
+
+// Plays a game using an ISMCTS solver with numIters iterations for each move
+// against an opponent that just picks random moves; returns the score for the
+// ISMCTS player.
+unsigned playGame(ISMCTS::SolverBase<Card> &solver, unsigned &callCounter, std::chrono::duration<double> &timeCounter)
+{
+    using namespace std;
+    using namespace std::chrono;
+
+    static mt19937 urng {random_device{}()};
+    KnockoutWhist game {2};
+
+    while (!game.validMoves().empty()) {
+        const auto p = game.currentPlayer();
+        Card move;
+        if (p == 0) {
+            const auto t0 = high_resolution_clock::now();
+            move = solver(game);
+            const auto t1 = high_resolution_clock::now();
+            ++callCounter;
+            timeCounter += t1 - t0;
+        } else {
+            const auto validMoves = game.validMoves();
+            uniform_int_distribution<size_t> randIdx {0, validMoves.size() - 1};
+            move = validMoves[randIdx(urng)];
+        }
+        game.doMove(move);
+    }
+    return game.getResult(0);
+}
 
 int main(int /*argc*/, char **/*argv*/)
 {
     using namespace std;
     using namespace std::chrono;
 
-    KnockoutWhist game {3};
-    const unsigned iters {500};
-    ISMCTS::SOSolver<Card> solver {iters};
+    const unsigned numGames {10000};
+    const unsigned numIters {100};
+    ISMCTS::SOSolver<Card> solver {numIters};
+    unsigned score {0};
+    unsigned totalCalls {0};
     duration<double> t;
-    unsigned iter_count {0};
 
-    while (!game.validMoves().empty()) {
-        std::cout << game << "\n";
-        const auto t0 = high_resolution_clock::now();
-        const auto move = solver(game);
-        const auto t1 = high_resolution_clock::now();
-        game.doMove(move);
-        t += t1 - t0;
-        iter_count += iters;
-    }
+    for (unsigned i = 0; i < numGames; ++i)
+        score += playGame(solver, totalCalls, t);
+
+    cout << "ISMCTS player won: " << score << " out of " << numGames << " games\n";
 
     const auto time_us = duration_cast<microseconds>(t).count();
-    cout << "Time taken: " << time_us << " microseconds" << endl;
-    cout << "Per iteration: " << double(time_us) / iter_count << endl;
+    cout << "Time taken: " << time_us << " microseconds\n";
+    cout << "Per iteration: " << double(time_us) / (totalCalls * numIters) << endl;
     return 0;
 }
