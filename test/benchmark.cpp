d7c78b01d3243a199fc58592cdb01fc21c2f91f0
Steven Franzen <sfranzen85@gmail.com>
2019-04-08 00:06:14 +0200

    Implement a first Catch2 test
    It consists of the old test executable code, modified to run as test
    cases.

---
+++ test/benchmark.cpp
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2019 Steven Franzen <sfranzen85@gmail.com>
+ * This file is subject to the terms of the MIT License; see the LICENSE file in
+ * the root directory of this distribution.
+ */
+#include "catch.hpp"
+#include <ismcts/sosolver.h>
+#include <ismcts/mosolver.h>
+#include "knockoutwhist.h"
+#include "card.h"
+
+#include <chrono>
+#include <vector>
+#include <iostream>
+
+using namespace std::chrono;
+
+class GameRunner
+{
+public:
+    GameRunner(ISMCTS::SolverBase<Card> &solver, unsigned int numGames)
+        : solver{solver}, numGames{numGames}
+    {}
+
+    void run()
+    {
+        for (unsigned i = 0; i < numGames; ++i)
+            score += playGame();
+        report();
+    }
+
+private:
+    ISMCTS::SolverBase<Card> &solver;
+    unsigned int numGames {100};
+    unsigned int score {0};
+    unsigned int numCalls {0};
+    duration<double> time {0};
+
+    unsigned int playGame()
+    {
+        KnockoutWhist game {2};
+
+        while (!game.validMoves().empty()) {
+            Card move;
+            if (game.currentPlayer() == 0) {
+                const auto t0 = high_resolution_clock::now();
+                move = solver(game);
+                time += high_resolution_clock::now() - t0;
+                ++numCalls;
+            } else {
+                move = game.validMoves().front();
+            }
+            game.doMove(move);
+        }
+        return game.getResult(0);
+    }
+    void report() const
+    {
+        using std::cout;
+        const auto time_us = duration_cast<microseconds>(time).count();
+        cout << std::string(79, '-') << "\nISMCTS player won: " << score << " out of " << numGames << " games,\n";
+        cout << "selecting " << numCalls << " moves in " << time_us << " microseconds.\n";
+        cout << "Average per search: " << double(time_us) / (numCalls) << " µs\n";
+        cout << std::string(79, '-') << "\n";
+    }
+};
+
+static const unsigned int numGames {100};
+
+TEMPLATE_PRODUCT_TEST_CASE("Fixed iterations benchmark", "[SOSolver]",
+                           ISMCTS::SOSolver, (Card, (Card, ISMCTS::RootParallel)))
+{
+    TestType solver {100};
+    GameRunner g(solver, numGames);
+    REQUIRE_NOTHROW(g.run());
+}
+
+TEMPLATE_PRODUCT_TEST_CASE("Fixed iterations benchmark", "[MOSolver]",
+                           ISMCTS::MOSolver, (Card, (Card, ISMCTS::RootParallel)))
+{
+    TestType solver {2, 100};
+    GameRunner g(solver, numGames);
+    REQUIRE_NOTHROW(g.run());
+}
--- test/main.cpp
+++ test/main.cpp
@@ -3,65 +3,7 @@
  * This file is subject to the terms of the MIT License; see the LICENSE file in
  * the root directory of this distribution.
  */
-#include <ismcts/sosolver.h>
-#include <ismcts/mosolver.h>
-#include "knockoutwhist.h"
-#include "card.h"
-
-#include <chrono>
-#include <vector>
-#include <random>
-#include <iostream>
-
-// Plays a game using an ISMCTS solver with numIters iterations for each move
-// against an opponent that just picks random moves; returns the score for the
-// ISMCTS player.
-unsigned playGame(ISMCTS::SolverBase<Card> &solver, unsigned &callCounter, std::chrono::duration<double> &timeCounter)
-{
-    using namespace std;
-    using namespace std::chrono;
-
-    static mt19937 urng {random_device{}()};
-    KnockoutWhist game {2};
-
-    while (!game.validMoves().empty()) {
-        const auto p = game.currentPlayer();
-        Card move;
-        if (p == 0) {
-            const auto t0 = high_resolution_clock::now();
-            move = solver(game);
-            const auto t1 = high_resolution_clock::now();
-            ++callCounter;
-            timeCounter += t1 - t0;
-        } else {
-            const auto validMoves = game.validMoves();
-            uniform_int_distribution<size_t> randIdx {0, validMoves.size() - 1};
-            move = validMoves[randIdx(urng)];
-        }
-        game.doMove(move);
-    }
-    return game.getResult(0);
-}
-
-int main(int /*argc*/, char **/*argv*/)
-{
-    using namespace std;
-    using namespace std::chrono;
-
-    const unsigned numGames {100};
-    const unsigned numIters {1};
-    ISMCTS::SOSolver<Card,ISMCTS::RootParallel> solver {milliseconds(5)};
-    unsigned score {0};
-    unsigned totalCalls {0};
-    duration<double> t;
-
-    for (unsigned i = 0; i < numGames; ++i)
-        score += playGame(solver, totalCalls, t);
-
-    cout << "ISMCTS player won: " << score << " out of " << numGames << " games\n";
-
-    const auto time_us = duration_cast<microseconds>(t).count();
-    cout << "Time taken: " << time_us << " microseconds\n";
-    cout << "Per iteration: " << double(time_us) / (totalCalls * numIters) << endl;
-    return 0;
-}
+// This file is used only to generate a Catch2 command line interface that will
+// run the tests
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
