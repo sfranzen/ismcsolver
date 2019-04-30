f5676255d1fd278d06864a1ef8152d21bd97d6c3
Steven Franzen <sfranzen85@gmail.com>
2019-04-30 13:51:52 +0200

    Make tester algorithm slightly more generic

--- test/benchmark.cpp
+++ test/benchmark.cpp
@@ -8,13 +8,17 @@
 #include "common/catch.hpp"
 #include "common/knockoutwhist.h"
 #include "common/card.h"
+#include "common/phantommnkgame.h"
 
 #include <chrono>
 #include <vector>
 #include <array>
+#include <map>
 #include <iostream>
+#include <iomanip>
 #include <random>
 #include <string>
+#include <cmath>
 
 namespace
 {
@@ -31,11 +35,14 @@ public:
         : m_numGames{numGames}
     {}
 
-    template<class Game, class Generator1, class Generator2>
-    void run(Game &&game, Generator1 &&g1, Generator2 &&g2)
+    template<class Game, class G1, class G2>
+    void run(Game &&game, G1 &&generator1, G2 &&generator2)
     {
-        for (unsigned i = 0; i < m_numGames; ++i)
-            m_score += playGame(game, g1, g2);
+        m_p0Scores.resize(m_numGames, 0);
+        m_numCalls.fill(0);
+        m_times.fill(Duration::zero());
+        for (auto &score : m_p0Scores)
+            score = playGame(game, generator1, generator2);
         report();
     };
 
@@ -44,19 +51,19 @@ private:
     using Duration = ExecutionPolicy::Duration;
 
     unsigned m_numGames;
-    double m_score {0};
-    std::array<unsigned, 2> m_numCalls {{0, 0}};
+    std::vector<double> m_p0Scores;
+    std::array<unsigned, 2> m_numCalls;
     std::array<Duration, 2> m_times;
 
-    template<class Game, class Generator1, class Generator2>
-    double playGame(Game &&game, Generator1 &&g1, Generator2 &&g2)
+    template<class Game, class G1, class G2>
+    double playGame(Game &&game, G1 &&generator1, G2 &&generator2)
     {
         auto newGame = game;
 
         while (!newGame.validMoves().empty()) {
             const auto player = newGame.currentPlayer();
             const auto t0 = Clock::now();
-            auto move = player == 0 ? g1(newGame) : g2(newGame);
+            auto move = player == 0 ? generator1(newGame) : generator2(newGame);
             m_times[player] += Clock::now() - t0;
             ++m_numCalls[player];
             newGame.doMove(move);
@@ -66,14 +73,31 @@ private:
 
     void report() const
     {
+        using namespace std;
         using namespace std::chrono;
-        using std::cout;
         static const auto separator = std::string(79, '-') + "\n";
-        cout << separator << "First player scored " << m_score << " points in " << m_numGames << " games.\n";
+
+        std::map<double,unsigned> counts;
+        for (auto score : m_p0Scores) {
+            auto ret = counts.emplace(score, 1);
+            if (!ret.second)
+                ++ret.first->second;
+        }
+
+        auto f = cout.flags();
+        auto p = cout.precision();
+        auto countWidth = std::floor(1 + std::log10(m_numGames));
+
+        cout << separator << "First player score stats after " << m_numGames << " games:\n";
+        for (auto &pair : counts)
+            cout << setw(3) << setprecision(2) << pair.first << ": " << setw(countWidth) << pair.second
+                << " times (" << setprecision(3) << pair.second * 100. / m_numGames << "%)\n";
+
+        cout << setiosflags(f) << setprecision(p);
         for (unsigned p : {0, 1}) {
             const auto time_us = duration_cast<microseconds>(m_times[p]).count();
             cout << "Player " << p << " selected " << m_numCalls[p] << " moves in " << time_us
-                 << " µs, average " << double(time_us) / m_numCalls[p] << " µs per move.\n";
+            << " µs, average " << double(time_us) / m_numCalls[p] << " µs per move.\n";
         }
         cout << separator;
     }
